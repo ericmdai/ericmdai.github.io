f3c0d463ba010ec94721213c58728388d482f108
Eric Dai <eric@hivemapper.com>
2016-09-17 20:23:25 -0700

    new template test

--- js/parallax.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.wrap').addEventListener('mousemove', function(e){
-  var x = (e.pageX * -1 / 2), y = (e.pageY * -1 / 2);
-  this.style.backgroundPosition = x + 'px ' + y + 'px';
-});
